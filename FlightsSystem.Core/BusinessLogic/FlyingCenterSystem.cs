87c88e2ea87fd4206065ea3df64c03c73ea6c624
Rezo Megrelidze <rezomegrelidze@gmail.com>
2020-08-07 15:49:20 +0300

    Added the functionality to archive flights and tickerts every three days

--- FlightsSystem.Core/BusinessLogic/FlyingCenterSystem.cs
+++ FlightsSystem.Core/BusinessLogic/FlyingCenterSystem.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
 using System.Threading;
 using System.Timers;
+using FlightsSystem.Core.DAL;
 using FlightsSystem.Core.Migrations;
 using Timer = System.Timers.Timer;
 
@@ -14,17 +17,52 @@ namespace FlightsSystem.Core.BusinessLogic
 
         static readonly List<Ticket> TicketHistory = new List<Ticket>();
         static readonly List<Flight> FlightHistory = new List<Flight>();
+        private ITicketDAO _ticketDAO;
+        private IFlightDAO _flightDAO;
+
         private FlyingCenterSystem()
         {
+            InitializeComponents();
+
             Timer timer = new Timer(TimeSpan.FromDays(3).TotalMilliseconds);
             timer.Start();
             timer.AutoReset = true;
             timer.Elapsed += TimerOnElapsed;
         }
 
+        private void InitializeComponents()
+        {
+            InitializeDAOs();
+        }
+
+        private void InitializeDAOs()
+        {
+            _ticketDAO = new TicketDAOEF();
+            _flightDAO = new FlightDAOEF();
+        }
+
+        public TFacade GetFacade<TFacade>() where TFacade : IAnonymousUserFacade, new()
+        {
+            return new TFacade();
+        }
+
         private void TimerOnElapsed(object sender, ElapsedEventArgs e)
         {
-            // code goes here
+            ArchiveTicketsAndFlights();
+        }
+
+        private void ArchiveTicketsAndFlights()
+        {
+            var ticketsToArchive = _ticketDAO.GetAll()
+                            .Where(t => t.Flight.LandingTime < (DateTime.Now.Subtract(TimeSpan.FromHours(3))));
+            var flightsToArchive = _flightDAO.GetAll()
+                            .Where(f => f.LandingTime < DateTime.Now.Subtract(TimeSpan.FromHours(3)));
+            TicketHistory.AddRange(ticketsToArchive);
+            FlightHistory.AddRange(flightsToArchive);
+            foreach (var ticket in ticketsToArchive)
+                _ticketDAO.Remove(ticket);
+            foreach(var flight in flightsToArchive)
+                _flightDAO.Remove(flight);
         }
     }
 }
--- FlightsSystem.Core/DAL/FlightsSystemContext.cs
+++ FlightsSystem.Core/DAL/FlightsSystemContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Core;
+using System.Data.Entity.Infrastructure;
 using FlightsSystem.Core.Login;
 
 namespace FlightsSystem.Core.DAL
